34d189e9cac357670e4c36b17c90608a4092a781
fengsim <jiping.xin@lsec.cc.ac.cn>
2026-04-27 14:14:26 +0800

    c

--- new.sh
+++ new.sh
@@ -5,3 +5,5 @@ cp src/_template/template_1.rst src/_$1/$1_1.rst
 cp src/_template/template_2.rst src/_$1/$1_2.rst
 cp src/_template/template_3.rst src/_$1/$1_3.rst
 cp src/_template/template_4.rst src/_$1/$1_4.rst
+rm src/template.rst
+rm -rf src/_template
