e0f8012a9c265febb2408c4e0d4c4578b7c4fc03
fengsim <jiping.xin@lsec.cc.ac.cn>
2026-04-27 14:03:42 +0800

    c

---
+++ new.sh
@@ -0,0 +1,6 @@
+cp src/template.rst src/$1.rst
+mkdir src/_$1
+cp src/_template/template_1.rst src/_$1/$1_1.rst
+cp src/_template/template_2.rst src/_$1/$1_2.rst
+cp src/_template/template_3.rst src/_$1/$1_3.rst
+cp src/_template/template_4.rst src/_$1/$1_4.rst
---
+++ src/template.rst
@@ -0,0 +1,13 @@
+######################
+Template
+######################
+
+.. Hidden TOCs
+
+.. toctree::
+   :maxdepth: 1
+
+   _template/template_1
+   _template/template_2
+   _template/template_3
+   _template/template_4
